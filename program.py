c7f7a6d0b814dd1648af31e5d7d23eab84613c18
gebhardt.lilla <gebhardt.lilla@ckik.hu>
2023-04-17 08:59:31 +0200

    Zoli elokeszites

--- program.py
+++ program.py
@@ -1,21 +1,26 @@
 def beolvas(nevek, hosszak, ertekelesek, csaladbaratok):
     fr=open("filmek.txt", "r")
     sor=fr.readline().strip().split()
-
     while sor != []:
         nevek.append(sor[0])
         hosszak.append(int(sor[1]))
         ertekelesek.append(float(sor[2]))
         csaladbaratok.append(int(sor[3]))
         sor=fr.readline().strip().split()
-      
     fr.close()
+
+def megadott_ido(nevek, hosszak):
+    ...
+
+def menu(nevek, hosszak, ertekelesek, csaladbaratok):
+    ...
+
 def main():
     nevek=[]
     hosszak=[]
     ertekelesek=[]
     csaladbaratok=[]
     beolvas(nevek, hosszak, ertekelesek, csaladbaratok)
-    print(nevek, hosszak, ertekelesek, csaladbaratok)
+    menu(nevek, hosszak, ertekelesek, csaladbaratok)
 
 main()
