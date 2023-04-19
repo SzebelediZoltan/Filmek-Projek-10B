2c6cd29971fbb6af7e1c48166fd6a49f721cb146
szebeledi.zoltan <szebeledi.zoltan@ckik.hu>
2023-04-19 08:14:19 +0200

    lista generalas

---
+++ lista_generalas.py
@@ -0,0 +1,27 @@
+from random import randint
+
+def beolvasas(nevek):
+    fr = open("film_nevek.txt", "r")
+    sor = fr.readline().strip()
+    while sor != "":
+        nevek.append(sor)
+        sor = fr.readline().strip()
+    fr.close()
+    
+def generalas(nevek):
+    fw = open("lista.txt", "w")
+    for i in range(len(nevek)):
+        nev = nevek[i]
+        hossz = randint(90, 210)
+        ertekeles = randint(2, 20) / 2
+        csaladbarat = randint(0, 1)
+        
+        fw.write(nev + " " + str(hossz) +" "+ str(ertekeles)+" "+str(csaladbarat)+"\n")
+    fw.close()
+
+def main():
+    nevek = []
+    beolvasas(nevek)
+    generalas(nevek)
+    
+main()
